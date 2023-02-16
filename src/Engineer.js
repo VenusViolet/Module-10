ce30394011a3f5f61ca892c76d33b92742b04bcc
Venus Rodin <VenusViolet1234@gmail.com>
2023-02-16 11:33:54 -1000

    engineer js

---
+++ src/Engineer.js
@@ -0,0 +1,13 @@
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, "Engineer", id, email);
+        this.github = github;
+    }
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;

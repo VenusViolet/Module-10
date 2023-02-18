f4a739392486f701cbc1757b92a7ac101d3e9206
Venus Rodin <VenusViolet1234@gmail.com>
2023-02-17 21:00:03 -1000

    name error

--- src/page-template.js
+++ src/page-template.js
@@ -6,7 +6,7 @@ module.exports = (team) => {
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <title>My Team</title>
+          <title>My Team!</title>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
           <link rel="stylesheet" href="style.css">
           <script src="https://kit.fontawesome.com/c502137733.js"></script>
@@ -15,7 +15,7 @@ module.exports = (team) => {
           <div class="container-fluid">
               <div class="row">
                   <div class="col-12 p-5 mb-3 team-heading">
-                      <h1 class="text-center">My Team</h1>
+                      <h1 class="text-center">My Team!</h1>
                   </div>
               </div>
           </div>
